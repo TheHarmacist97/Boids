a623abd9b9d5508273b6f6046b59cbcd00042e97
Varun A. Vyas <varunvyas55@gmail.com>
2024-05-14 15:22:31 +0530

    Jobs basic implementation

--- Assets/Scripts/JobsTest.cs
+++ Assets/Scripts/JobsTest.cs
@@ -6,94 +6,69 @@ using UnityEngine;
 using Random = Unity.Mathematics.Random;
 public class JobsTest : MonoBehaviour
 {
-    [SerializeField] bool useCollated;
+    [SerializeField] bool useJobs;
     [SerializeField] int length;
+    private Random rng;
+    private JobHandle jobHandle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uint seed = 9421;
+        rng = new(seed);
     }
 
     private void Update()
     {
-        uint seed = 9421;
-        Random rng = new(seed);
-
         float startTime = Time.realtimeSinceStartup;
-        if (useCollated)
+        if (useJobs)
         {
-            NativeArray<float> result = new(1, Allocator.TempJob);
-            result[0] = 0;
-            NativeArray<float2> vectorArray = new(length, Allocator.TempJob);
-            for (int i = 0; i < length; i++)
+            NativeArray<JobHandle> jobs = new(10,Allocator.Temp);
+            for (int i = 0; i<10; i++)
             {
-                vectorArray[i] = rng.NextFloat2();
+                jobs[i] = (ScheduleToughJob());
             }
-            SimpleCollatedJob sJob = new()
-            {
-                a = vectorArray,
-                result = result
-            };
-            JobHandle jHandle = sJob.Schedule();
-            jHandle.Complete();
-            Debug.Log("Collated time " + ((Time.realtimeSinceStartup - startTime) * 1000f) + " ms " + sJob.result[0]);
-
-            vectorArray.Dispose();
-            result.Dispose();
+            JobHandle.CompleteAll(jobs);
+            jobs.Dispose();
         }
         else
         {
-            NativeArray<NativeArray<float>> nestedResultArray = new(length, Allocator.Temp);
-            NativeArray<JobHandle> jobList = new(length, Allocator.Temp);
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < 10; i++)
             {
-                nestedResultArray[i] = new NativeArray<float>(1, Allocator.TempJob);
-                jobList[i] = new SimpleDistributedJob(rng.NextFloat2(), nestedResultArray[i]).Schedule();
+                ToughSyncedOperation();
             }
-            JobHandle.CompleteAll(jobList);
-            float endResult = 0.0f;
-            //NativeArray<float> copy;
-            for (int i = 0; i < length; i++)
-            {
-                endResult += nestedResultArray[i][0];
-                nestedResultArray[i].Dispose();
-            }
-            Debug.Log("Distributed Time: " + ((Time.realtimeSinceStartup - startTime) * 1000f) + " ms " + endResult);
-            nestedResultArray.Dispose();
+        }
+        Debug.Log("With"+ (useJobs?" Jobs: ": "out Jobs: ") + ((Time.realtimeSinceStartup - startTime) * 1000f) + " ms");
+    }
+
+    private void ToughSyncedOperation()
+    {
+        float value = 0f;
+        for (int i = 0; i < length; i++)
+        {
+            value = math.exp10(math.sqrt(value));
         }
     }
-}
 
-[BurstCompile]
-public struct SimpleDistributedJob : IJob
-{
-    [ReadOnly]
-    public float2 a;
-    public NativeArray<float> result;
-
-    public SimpleDistributedJob(float2 a, NativeArray<float> result)
+    private JobHandle ScheduleToughJob()
     {
-        this.a = a;
-        this.result = result;
-    }
-
-    public void Execute()
-    {
-        result[0] += a.x + a.y;
-    }
-}
-[BurstCompile]
-public struct SimpleCollatedJob : IJob
-{
-    [ReadOnly]
-    public NativeArray<float2> a;
-    public NativeArray<float> result;
-    public void Execute()
-    {
-        result[0] = 0f;
-        foreach (float2 item in a)
+        JobStruct job = new JobStruct()
         {
-            result[0] += item.x + item.y;
+            length = length
+        };
+        return job.Schedule();
+    }
+}
+
+[BurstCompile]
+public struct JobStruct : IJob
+{
+    public int length;
+    public void Execute()
+    {
+        float value = 0f;
+        for (int i = 0;i < length;i++)
+        {
+            value = math.exp10(math.sqrt(value));
         }
     }
 }
